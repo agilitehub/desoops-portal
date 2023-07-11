55c4bd7bcf6ea7525196c9c857d9b127f1bad4c0
John Jardin <john@agilite.io>
2023-07-11 13:09:21 +0200

    In Distribution Dashboard controller, created functions to setupHodlers and calculatePercentages

--- src/custom/modules/DistributionDashboard/controller.js
+++ src/custom/modules/DistributionDashboard/controller.js
@@ -3,6 +3,82 @@ import Axios from 'agilite-utils/axios'
 // Utils
 import Enums from '../../lib/enums'
 import { getDeSo } from '../../lib/deso-controller'
+import { hexToInt } from '../../lib/utils'
+
+export const setupHodlers = (desoProfile, distributeTo) => {
+  return new Promise((resolve, reject) => {
+    let allHodlers = []
+    let tmpHodlers = null
+    let coinAmount = 0
+    let coinTotal = 0
+
+    try {
+      // If user selects DAO or Creator Coin Hodlers, we need to get the relevant users
+      switch (distributeTo) {
+        case Enums.values.DAO:
+          tmpHodlers = JSON.parse(JSON.stringify(desoProfile.daoHodlers))
+          break
+        case Enums.values.CREATOR:
+          tmpHodlers = JSON.parse(JSON.stringify(desoProfile.ccHodlers))
+          break
+      }
+
+      // Calculate Coin Total and valid Hodlers
+      tmpHodlers.map((entry) => {
+        // Ignore entry if it does not have a Profile OR if it is the same as current logged in user
+        if (entry.ProfileEntryResponse && entry.ProfileEntryResponse.Username !== desoProfile.username) {
+          // Set Defaults
+          entry.status = Enums.values.EMPTY_STRING
+
+          // Determine Number of Coins based on the Coin Type (value)
+          if (distributeTo === Enums.values.DAO) {
+            coinAmount = entry.BalanceNanosUint256
+            coinAmount = hexToInt(coinAmount)
+            coinAmount = coinAmount / Enums.values.NANO_VALUE / Enums.values.NANO_VALUE
+          } else {
+            coinAmount = entry.BalanceNanos
+            coinAmount = coinAmount / Enums.values.NANO_VALUE
+          }
+
+          entry.coinAmount = coinAmount
+          entry.percentOwnership = coinTotal += coinAmount
+          allHodlers.push(entry)
+        }
+
+        return null
+      })
+
+      calculatePercentages(allHodlers, coinTotal, 'desc', 3)
+        .then((result) => {
+          allHodlers = result
+          resolve({ allHodlers, coinTotal })
+        })
+        .catch((e) => {
+          reject(e)
+        })
+    } catch (e) {
+      reject(e)
+    }
+  })
+}
+
+const calculatePercentages = (hodlers, coinTotal, sortOrder = 'desc', decimalPlaces) => {
+  return new Promise((resolve, reject) => {
+    const result = hodlers.map((entry) => {
+      const percentage = (entry.coinAmount / coinTotal) * 100
+      const formattedCoinAmount = decimalPlaces != null ? entry.coinAmount.toFixed(decimalPlaces) : entry.coinAmount
+      return { ...entry, percentage, coinAmount: formattedCoinAmount }
+    })
+
+    if (sortOrder === 'asc') {
+      result.sort((a, b) => a.percentage - b.percentage)
+    } else if (sortOrder === 'desc') {
+      result.sort((a, b) => b.percentage - a.percentage)
+    }
+
+    resolve(result)
+  })
+}
 
 export const getHodlers = (Username, IsDAOCoin, fetchHodlings) => {
   return new Promise((resolve, reject) => {
